6a0f2ba19b65ad58cd9bf7c003a92aa232ae75ca
wswiatkowski <werty240@gmail.com>
2016-07-27 16:06:33 +0200

    First approach on problem using itertools

---
+++ task1.py
@@ -0,0 +1,19 @@
+from itertools import combinations
+
+
+def foo(tab):
+    '''
+    Function counts number of subsets in given array;
+    each subset has maximum element which is the sum of all the other elements of subset.
+    :param tab: Array to find desired subsets in.
+    :return ret: Number of desired subsets found in given array.
+    '''
+
+    ret = 0
+    for i in range(2, len(tab)):
+        for comb in combinations(tab, i):
+            print comb
+            if sum(comb) in tab:
+                ret += 1
+
+    return ret
