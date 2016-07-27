68fb4383fa4f5e06c6ed463c1efd71d5ad2c7212
wswiatkowski <werty240@gmail.com>
2016-07-27 16:46:22 +0200

    minor fixes

--- task1.py
+++ task1.py
@@ -2,17 +2,17 @@ from itertools import combinations
 
 
 def foo(tab):
-    '''
+    """
     Function counts number of subsets in given array;
     each subset has maximum element which is the sum of all the other elements of subset.
     :param tab: Array to find desired subsets in.
     :return ret: Number of desired subsets found in given array.
-    '''
-
+    """
     ret = 0
+
     for i in range(2, len(tab)):
         for comb in combinations(tab, i):
-            print comb
+            print comb, sum(comb)
             if sum(comb) in tab:
                 ret += 1
 
